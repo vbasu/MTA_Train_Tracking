49695ee9dd7dc497f4a603ee97aadccb245c1ce1
vbasu <ved.basu@gmail.com>
2023-04-23 16:35:57 -0400

    Add data and notebook:
    Signed-off-by: vbasu <ved.basu@gmail.com>

--- constants.py
+++ constants.py
@@ -4,4 +4,14 @@ SUBWAY_FEEDS = {
     "E": "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs"
 }
 
+URLS_ALL_LINES = [
+    'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace',
+    'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm',
+    'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g',
+    'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-jz',
+    'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw',
+    'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l',
+    'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs',
+]
+
 API_KEY = "41DRpw8f3b8za42bqeIoiaROXXBM1eKR3FLVIQQX"
